62cc7692c285fd8c3f58e57f886238de7b0264a9
Ajju4318 <analystajju@gamil.com>
2024-09-06 18:16:34 +0530

    python

---
+++ Day-02/examples/test.py
@@ -0,0 +1,23 @@
+arn = "arn:aws:iam::123456789012:user/johndoe"
+
+print(arn.split("/"))
+print(arn.split("/")[0])
+print(arn.split("/")[1])
+
+name = "ajju shaik"
+print(name.upper())
+
+a = "razi"
+b = "ajju"
+abc = a + " " + b
+print(abc)
+print(abc.upper())
+
+a = 410
+b = 590
+abc = a+b
+print(abc)
+
+message =  "ajju loves razi and they are getting marries very soon"
+length = len(message)
+print("length of message:", length)
